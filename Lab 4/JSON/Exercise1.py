29e228abc48002cc4bbdd3e5ebee072683548507
Asan <as_serikbay@kbtu.kz>
2025-02-20 00:05:16 +0500

    Lab 4

--- Lab 4/JSON/Exercise1.py
+++ Lab 4/JSON/Exercise1.py
@@ -1,19 +1,19 @@
 import json
 file_path = r"C:\Users\Lenovo Gaming\Desktop\PP\PP2\Lab 4\Json\sample-data.json"
-with open(file_path, "r") as file:
+with open(file_path, "r", encoding="utf-8") as file:
     data = json.load(file)
-    
-interfaces = data["interfaces"] 
 
+interfaces = data["imdata"]
 print("Interface Status")
-print("=" * 50)
-print("{:<50} {:<15} {:<10} {:<10}".format("DN", "Description", "Speed", "MTU"))
-print("-" * 50)
-
+print("=" * 80)
+print("{:<50} {:<20} {:<10} {:<10}".format("DN", "Description", "Speed", "MTU"))
+print("-" * 80)
 for interface in interfaces:
-    dn = interface["dn"]
-    description = interface["description"]
-    speed = interface["speed"]
-    mtu = interface["mtu"]
+    attributes = interface["l1PhysIf"]["attributes"]
 
-    print("{:<50} {:15} {:<10} {:<10}".format(dn, description, speed, mtu))
+    dn = attributes["dn"]
+    description = attributes.get("descr", "") 
+    speed = attributes.get("speed", "inherit") 
+    mtu = attributes.get("mtu", "N/A") 
+
+    print("{:<50} {:<20} {:<10} {:<10}".format(dn, description, speed, mtu))
